85b341c0e94f4d5985b5380048d639d7341599d4
codersoum <150548307+codersoum@users.noreply.github.com>
2024-11-23 23:03:03 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -5,7 +5,7 @@ public class Prime {
 	public static void main(String[] args)
 	{
 	  Scanner sc= new Scanner(System.in);
-	  int n=sc.nextInt();
+	  int n=100;
 	  if(checkprime(n))
 	  System.out.println(n+"is a prime number");
 	  else
